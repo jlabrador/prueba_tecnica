5fc463cffc44b2f2f89958343fb4ac4e433fceca
jlabrador <jose.labrador.gonzalez@gmail.com>
2017-05-13 15:14:26 +0200

    se cambia de estado al enviar

--- src/AppBundle/Command/EnviarPruebaTecnicaCommand.php
+++ src/AppBundle/Command/EnviarPruebaTecnicaCommand.php
@@ -28,7 +28,6 @@ class EnviarPruebaTecnicaCommand extends ContainerAwareCommand
         $this->output = $output;
         $em = $this->getContainer()->get('doctrine')->getManager();
         $candidatos = $em->getRepository('AppBundle:Candidato')->findByFechaHoraPrueba(new \DateTime('now'));
-        $candidatos = $em->getRepository('AppBundle:Candidato')->findAll();
         /** @var Candidato $candidato */
         foreach ($candidatos as $candidato){
 
